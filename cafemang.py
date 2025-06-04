7f7506f6857d3c0d1c5599de7542b061d3d73a29
vaishnavi nandne <vaishnavirnandne@gmail.com>
2025-06-04 22:56:55 +0530

    feature updated

--- cafemang.py
+++ cafemang.py
@@ -11,13 +11,11 @@ print("Welcome to Aditi's Restaurant")
 print("Menu:")
 print("pizza : Rs 40 \n pasta : Rs 50 \n burger : Rs 60 \n salad : Rs 70 \n cofee : Rs 80")
 
-
-order_total = 0     #user enter prize added on this var suppose salad then cofee 70+80=150
+# Initialize total order amount
+order_total = 0
 
 # First item
-item_1 = input("Enter the name of the item you want to order: ")    #check conditions the choice entered by user is available
-                                                              #in our restaurent or not if yes then go further if not then 
-                                                              #it should display not available  
+item_1 = input("Enter the name of the item you want to order: ")
 
 # Check if item_1 is available
 if item_1 in menu:
@@ -31,8 +29,7 @@ another_order = input("Do you want to add another item? (yes/no): ")
 
 if another_order == "yes":
     item_2 = input("Enter the name of the second item: ")
-    if item_2 in menu:      #membership operator we use here to check users input avalable or not
-
+    if item_2 in menu:
         order_total += menu[item_2]
         print(f"Item '{item_2}' has been added to your order.")
     else:
